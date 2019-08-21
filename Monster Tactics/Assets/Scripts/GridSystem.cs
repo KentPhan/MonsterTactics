a9d720be3f42f896b869e0dcb5fe8a300827a155
Sumi <sumi.n.lcy@gmail.com>
2019-08-20 20:08:17 -0600

    Player script is added, working on Inventory system and nav mesh stuff

--- Monster Tactics/Assets/Scripts/GridSystem.cs
+++ Monster Tactics/Assets/Scripts/GridSystem.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class GridSystem : MonoBehaviour
 {
-    [System.NonSerialized] public Vector3 mousePosition;
+    private static GridSystem _instance;
+    public static GridSystem Instance { get { return _instance; } }
+
+    // Added last clicked and last hovered position
+    [System.NonSerialized] public Vector3 enteringGridPosition;
+    [System.NonSerialized] public Vector3 clickedGridPosition;
     [System.NonSerialized] public List<Square> squares = new List<Square>();
 
     void Awake()
     {
+        if(_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
         //Creates a master list of all squares in this grid system
         foreach (Square square in GetComponentsInChildren<Square>())
             squares.Add(square);
---
+++ Monster Tactics/Assets/Scripts/Inventory System/Items/Sword.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Item
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxStack = 1;
+    }
+
+    public override void Equip()
+    {
+        base.Equip();
+    }
+}
