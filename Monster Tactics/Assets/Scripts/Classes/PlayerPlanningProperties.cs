1d4b02151edb7e3f32c2949cdf99fb29c5d6c681
KentPhan <kent.chi.lam@gmail.com>
2019-08-20 20:14:11 -0600

    Added Player Planning Properties

---
+++ Monster Tactics/Assets/Scripts/Classes/PlayerPlanningProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Classes
+{
+    public struct PlayerPlanningProperties
+    {
+        public Vector3 PlannedPosition;
+        public int UsableActionPoints;
+    }
+}
