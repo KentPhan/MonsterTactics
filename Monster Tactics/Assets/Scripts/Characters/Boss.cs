47e81bc6cda3a4102b82b7f044c33edfffae0054
KentPhan <kent.chi.lam@gmail.com>
2019-08-28 00:08:00 -0600

    Added Basic Boss Occupation stuff

--- Monster Tactics/Assets/Scripts/Characters/Boss.cs
+++ Monster Tactics/Assets/Scripts/Characters/Boss.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Assets.Scripts.Constants;
 using UnityEngine;
 
 namespace Assets.Scripts.Characters
@@ -5,11 +7,20 @@ namespace Assets.Scripts.Characters
     public class Boss : MonoBehaviour, ICharacter
     {
         [SerializeField] [Range(1, 100)] private int health;
+
+        [SerializeField] private List<GameObject> gameObjectsWithMarkers;
         
         // Start is called before the first frame update
         void Start()
         {
-
+            // Foreach square raycast downwards and mark that square as occupied
+            foreach (GameObject markerObject in gameObjectsWithMarkers)
+            {
+                foreach (SquareMarker marker in markerObject.GetComponentsInChildren<SquareMarker>())
+                {
+                    FindAndMarkSquare(marker);
+                }
+            }
         }
 
         // Update is called once per frame
@@ -32,6 +43,36 @@ namespace Assets.Scripts.Characters
         public void TakeDamage(int damage)
         {
             throw new System.NotImplementedException();
+        }
+
+        private void FindAndMarkSquare(SquareMarker marker)
+        {
+            // Raycast downward to find current square
+            bool squareFound = true;
+            int squareMask = LayerMask.GetMask(Layers.GRID);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(marker.transform.position, Vector3.down, out hitInfo, 5000, squareMask,
+                QueryTriggerInteraction.UseGlobal))
+            {
+                Square foundSquare = hitInfo.transform.GetComponent<Square>();
+                if (foundSquare == null)
+                {
+                    squareFound = false;
+                }
+                else
+                {
+                    foundSquare.SetNotTraversable();
+                }
+            }
+            else
+            {
+                squareFound = false;
+            }
+
+            if (!squareFound)
+            {
+                Debug.LogError("No Square found below marker for the monster");
+            }
         }
     }
 }
--- Monster Tactics/Assets/Scripts/Square.cs
+++ Monster Tactics/Assets/Scripts/Square.cs
@@ -1,101 +1,141 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Square : MonoBehaviour
+namespace Assets.Scripts
 {
-    [SerializeField] protected Color none = Color.clear, peek = Color.cyan, traversible = Color.yellow, select = Color.green, traversing = Color.blue;
-
-    public List<Square> neighbors = new List<Square>();
-
-    [SerializeField] protected MeshRenderer meshRenderer;
-    [SerializeField] protected GameObject loot, danger;
-    protected LineRenderer lineRenderer;
-    protected int steps = int.MaxValue;
-
-    private void Awake()
+    public class Square : MonoBehaviour
     {
-        lineRenderer = GetComponent<LineRenderer>();
-        meshRenderer.material.color = Color.clear;
-    }
-
-    private void Start()
-    {
-        if (GridSystem.Instance.theMap.ContainsKey(transform.localPosition + Vector3.forward))
-            neighbors.Add(GridSystem.Instance.theMap[transform.localPosition + Vector3.forward]);
-        if (GridSystem.Instance.theMap.ContainsKey(transform.localPosition + Vector3.back))
-            neighbors.Add(GridSystem.Instance.theMap[transform.localPosition + Vector3.back]);
-        if (GridSystem.Instance.theMap.ContainsKey(transform.localPosition + Vector3.left))
-            neighbors.Add(GridSystem.Instance.theMap[transform.localPosition + Vector3.left]);
-        if (GridSystem.Instance.theMap.ContainsKey(transform.localPosition + Vector3.right))
-            neighbors.Add(GridSystem.Instance.theMap[transform.localPosition + Vector3.right]);
-    }
-
-    private void OnMouseEnter()
-    {
-        if (meshRenderer.material.color == none)
+        public enum OccupationState
         {
-            GridSystem.Instance.hoveringSquare = this;
-            meshRenderer.material.color = peek;
+            NONE,
+            BOSS
         }
-        else if (meshRenderer.material.color == traversible)
-        {
-            GridSystem.Instance.hoveringSquare = this;
-            meshRenderer.material.color = select;
-        }
-    }
 
-    private void OnMouseOver()
-    {
-        if (Input.GetKeyDown("d"))
-        {
-            Instantiate(danger, transform.position, Quaternion.identity);
-        }
-        else if (Input.GetKeyDown("l"))
-        {
-            Instantiate(loot, transform.position, Quaternion.identity);
-        }
-    }
+        [SerializeField] protected Color none = Color.clear,
+            peek = Color.cyan,
+            traversible = Color.yellow,
+            select = Color.green,
+            traversing = Color.blue,
+            intraversable = new Color(0.8f,0,0),
+            intraversableHighlight = Color.red;
 
-    private void OnMouseExit()
-    {
-        if (meshRenderer.material.color == peek)
-            meshRenderer.material.color = none;
-        else if (meshRenderer.material.color == select)
-            meshRenderer.material.color = traversible;
-    }
+        public List<Square> neighbors = new List<Square>();
 
-    public void Range(int range, int isteps = 0)
-    {
-        steps = isteps;
-        meshRenderer.material.color = traversible;
-        if (range > 0)
+        [SerializeField] protected MeshRenderer meshRenderer;
+        [SerializeField] protected GameObject loot, danger;
+        protected LineRenderer lineRenderer;
+        protected int steps = int.MaxValue;
+
+        private OccupationState occupationState;
+
+        private void Awake()
         {
-            foreach (Square square in neighbors)
+            lineRenderer = GetComponent<LineRenderer>();
+            meshRenderer.material.color = Color.clear;
+            this.occupationState = OccupationState.NONE;
+        }
+
+        private void Start()
+        {
+            if (GridSystem.Instance.theMap.ContainsKey(transform.localPosition + Vector3.forward))
+                neighbors.Add(GridSystem.Instance.theMap[transform.localPosition + Vector3.forward]);
+            if (GridSystem.Instance.theMap.ContainsKey(transform.localPosition + Vector3.back))
+                neighbors.Add(GridSystem.Instance.theMap[transform.localPosition + Vector3.back]);
+            if (GridSystem.Instance.theMap.ContainsKey(transform.localPosition + Vector3.left))
+                neighbors.Add(GridSystem.Instance.theMap[transform.localPosition + Vector3.left]);
+            if (GridSystem.Instance.theMap.ContainsKey(transform.localPosition + Vector3.right))
+                neighbors.Add(GridSystem.Instance.theMap[transform.localPosition + Vector3.right]);
+        }
+
+        private void OnMouseEnter()
+        {
+            if (meshRenderer.material.color == none)
             {
-                if ((steps + 1) < (square.steps))
-                    square.Range(range - 1, steps + 1);
+                GridSystem.Instance.hoveringSquare = this;
+                meshRenderer.material.color = peek;
+            }
+            else if (meshRenderer.material.color == traversible)
+            {
+                GridSystem.Instance.hoveringSquare = this;
+                meshRenderer.material.color = select;
+            }
+            else if (meshRenderer.material.color == intraversable)
+            {
+                GridSystem.Instance.hoveringSquare = this;
+                meshRenderer.material.color = intraversableHighlight;
             }
         }
-    }
 
-    public bool IsInRange()
-    {
-        return meshRenderer.material.color == select;
-    }
+        private void OnMouseOver()
+        {
+            if (Input.GetKeyDown("d"))
+            {
+                Instantiate(danger, transform.position, Quaternion.identity);
+            }
+            else if (Input.GetKeyDown("l"))
+            {
+                Instantiate(loot, transform.position, Quaternion.identity);
+            }
+        }
 
-    public int ActionPointCost()
-    {
-        return steps;
-    }
+        private void OnMouseExit()
+        {
+            if (meshRenderer.material.color == peek)
+                meshRenderer.material.color = none;
+            else if (meshRenderer.material.color == select)
+                meshRenderer.material.color = traversible;
+            else if(meshRenderer.material.color == intraversableHighlight)
+                meshRenderer.material.color = intraversable;
+        }
 
-    public void Clear()
-    {
-        steps = int.MaxValue;
-        meshRenderer.material.color = none;
-        foreach (Square square in neighbors)
-            if (square.meshRenderer.material.color == traversible || square.meshRenderer.material.color == select)
-                square.Clear();
-    }
+        public void Range(int range, int isteps = 0)
+        {
+            if (meshRenderer.material.color == intraversable || meshRenderer.material.color == intraversableHighlight)
+                return;
+            steps = isteps;
+            meshRenderer.material.color = traversible;
+            if (range > 0)
+            {
+                foreach (Square square in neighbors)
+                {
+                    if ((steps + 1) < (square.steps))
+                        square.Range(range - 1, steps + 1);
+                }
+            }
+        }
 
+        public bool IsInRange()
+        {
+            return meshRenderer.material.color == select;
+        }
+
+        public int ActionPointCost()
+        {
+            return steps;
+        }
+
+        public void Clear()
+        {
+            if (meshRenderer.material.color == intraversable || meshRenderer.material.color == intraversableHighlight)
+                return;
+
+            steps = int.MaxValue;
+            meshRenderer.material.color = none;
+            foreach (Square square in neighbors)
+                if (square.meshRenderer.material.color == traversible || square.meshRenderer.material.color == select || square.meshRenderer.material.color == intraversable)
+                    square.Clear();
+        }
+        
+        public void SetNotTraversable()
+        {
+            meshRenderer.material.color = intraversable;
+        }
+
+        public void SetTraversable()
+        {
+            meshRenderer.material.color = none;
+        }
+
+        
+    }
 }
